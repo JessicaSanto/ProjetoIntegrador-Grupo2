e474b20549e671fa47204a5d5cd6c958e6b94870
Jessica Franzon Cruz do Espirito Santo <jessica.santo@sp.senai.br>
2025-09-26 21:27:46 -0300

    projeto

--- main.py
+++ main.py
@@ -531,7 +531,7 @@ def gera_resposta(status, nome_conteudo, conteudo, mensagem=False):
 #         mydb.create_all()  # Cria as tabelas no banco de dados
     
 start_mqtt()
-app.run(port=5000, host='localhost', debug=True)
+app.run(port=5000, host='0.0.0.0', debug=True)
 
 
 
