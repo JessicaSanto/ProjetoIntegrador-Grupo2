5e4a3d590be6525e7a10446cc2e8b3ca1f79ea6b
Jessica Franzon Cruz do Espirito Santo <jessica.santo@sp.senai.br>
2025-10-03 13:44:47 -0300

    atualiza

--- main.py
+++ main.py
@@ -526,8 +526,8 @@ def gera_resposta(status, nome_conteudo, conteudo, mensagem=False):
         body['mensagem'] = mensagem
     return Response(json.dumps(body), status=status, mimetype='application/json')
 
-if __name__ == '__main__':
-    with app.app_context():
-        mydb.create_all()
-    start_mqtt()
-    app.run(port=5000, host='0.0.0.0', debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         mydb.create_all()
+start_mqtt()
+app.run(port=5000, host='0.0.0.0', debug=True)
